d9d83385bbd202c66fd75864b3b97043ef4e71c0
jeeva <jeevaathecoder@gmail.com>
2023-03-05 15:25:00 +0530

    VER Y NEW

---
+++ New.java
@@ -0,0 +1,14 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println("in th new jeeva db");
+
+        No obj=new No();
+        System.out.println(obj.c);
+    }
+}
+class No{
+    int a=10;
+    int b=4;
+    int c=a+b;
+
+        }
